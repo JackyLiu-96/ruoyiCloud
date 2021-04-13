c349a5ca9c38398dc606687ffc84def58073c7ba
JackyLiu-96 <2449767269@qq.com>
2021-04-13 09:43:03 +0800

    若依微服务

---
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/config/GatewayConfig.java
@@ -0,0 +1,57 @@
+package com.ruoyi.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.ruoyi.gateway.handler.SentinelFallbackHandler;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 网关限流配置
+ * 
+ * @author ruoyi
+ */
+@Configuration
+public class GatewayConfig
+{
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelFallbackHandler sentinelGatewayExceptionHandler()
+    {
+        return new SentinelFallbackHandler();
+    }
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter sentinelGatewayFilter()
+    {
+        return new SentinelGatewayFilter();
+    }
+    @PostConstruct
+    public void doInit()
+    {
+        // 加载网关限流规则
+        initGatewayRules();
+    }
+
+    /**
+     * 网关限流规则
+     */
+    private void initGatewayRules()
+    {
+        Set<GatewayFlowRule> rules = new HashSet<>();
+        rules.add(new GatewayFlowRule("ruoyi-system")
+                .setCount(3) // 限流阈值
+                .setIntervalSec(60)); // 统计时间窗口，单位是秒，默认是 1 秒
+        // 加载网关限流规则
+        GatewayRuleManager.loadRules(rules);
+    }
+}
